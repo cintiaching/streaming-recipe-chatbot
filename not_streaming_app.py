6beeef82f0c4b4b79993d6fb4e0ccf406e3c020d
cintiaching <cintiaching@lcjgroup.com>
2025-05-20 15:27:29 +0800

    add logging

--- not_streaming_app.py
+++ not_streaming_app.py
@@ -1,37 +1,25 @@
 import os
+import logging
 import time
 
 from fastapi import FastAPI
 from together import Together
 from dotenv import load_dotenv
 
+from streaming_app import fake_search_recipes, fake_get_nutrition, fake_save_to_db
+
 load_dotenv()
 app = FastAPI()
 client = Together()
 
-
-def fake_search_recipes(dish: str):
-    time.sleep(1.2)
-    return None
-
-
-def fake_get_nutrition(dish: str):
-    """Simulate "nutrition analysis" without real APIs"""
-    nutrition = {"pasta": "450 kcal | 32g carbs", "salad": "120 kcal | 5g carbs"}.get(
-        dish.lower(), "500 kcal | 40g carbs"
-    )
-    return f"\n📊 Nutrition (approx): {nutrition}\n"
-
-
-def fake_save_to_db(dish: str, generated_recipe: str):
-    time.sleep(2)
-    print(f"saved {dish}: {generated_recipe} to db")
+logger = logging.getLogger("uvicorn.error")
+logger.setLevel(logging.INFO)
 
 
 @app.get("/recipe/not_stream/")
 def stream_recipe(dish: str):
+    start_time = time.time()
     fake_search_recipes(dish)
-    time.sleep(1.2)
 
     # LLM NOT STREAMING
     response = client.chat.completions.create(
@@ -46,11 +34,10 @@ def stream_recipe(dish: str):
     full_recipe = response.choices[0].message.content
 
     nutrition = fake_get_nutrition(dish)
-    time.sleep(0.5)
 
     # BACKGROUND TASK (blocking return)
     fake_save_to_db(dish, full_recipe)
-
+    logger.info(f"Everything took {time.time() - start_time:.2f} seconds")
     return {
         "full_recipe": full_recipe,
         "nutrition": nutrition,
